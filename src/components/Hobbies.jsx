3623aa54ee8c7cdbff2dcf8e137b552e4581ae8f
Edgar Méndez <mdzredgar@gmail.com>
2023-01-17 22:12:04 -0600

    Correciones.

--- src/components/Hobbies.jsx
+++ src/components/Hobbies.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const Hobbies = () => {
   const hobbies = ['Leer', 'Ver series', 'Ejercicio', 'Investigar', '']
-  
+
   return (
     <div className="card">
         <h2>Hobbies</h2>
         <ul>
-          <li>{person.hobbies[0]}</li>
-          <li>{person.hobbies[1]}</li>
-          <li>{person.hobbies[2]}</li>
-          <li>{person.hobbies[3]}</li>
-          <li>{person.hobbies[4]}</li>
+          <li>{hobbies[0]}</li>
+          <li>{hobbies[1]}</li>
+          <li>{hobbies[2]}</li>
+          <li>{hobbies[3]}</li>
+          <li>{hobbies[4]}</li>
         </ul>
       </div>
   )
--- src/components/StackTech.jsx
+++ src/components/StackTech.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
 function StackTech() {
+  const stack = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js']
   return (
     <div className="card">
         <h2>Mi Stack Tech</h2>
         <ul>
-          <li>{person.stack[0]}</li>
-          <li>{person.stack[1]}</li>
-          <li>{person.stack[2]}</li>
-          <li>{person.stack[3]}</li>
-          <li>{person.stack[4]}</li>
+          <li>{stack[0]}</li>
+          <li>{stack[1]}</li>
+          <li>{stack[2]}</li>
+          <li>{stack[3]}</li>
+          <li>{stack[4]}</li>
         </ul>
       </div>
   )
