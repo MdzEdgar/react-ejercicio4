fc392a782a2ec17aa4c77f0f4a0344d091e55d8d
Edgar Méndez <mdzredgar@gmail.com>
2023-01-17 22:09:53 -0600

    Integrando los componentes en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,24 @@
 import './App.css'
 import FavoriteFood from './components/FavoriteFood'
+import Hobbies from './components/Hobbies';
+import Personal from './components/Personal';
+import StackTech from './components/StackTech';
 
 function App() {
-  const person = {
-    name: 'Edgar Méndez',
-    age: 31,
-    favoriteMovie: "Reservoir Dogs",
-    favoriteMusic: "Clasica",
-    hobbies: ['Leer', 'Ver series', 'Ejercicio', 'Investigar', ''],
-    comidas: ['Tacos' , 'Hamburguesa de pollo', 'Carne asada', 'Mariscos', 'Chocolate'],
-    stack: ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js']
-  }
+  
+
+  //Hacer un componente por cada tarjeta/card
 
   return (
     <div className="App">
       <div className="container">
-      <div className="card">
-      <ul>
-        <h2>Personal</h2>
-        <li>{person.name}</li>
-        <li>{person.age} años</li>
-        <li>{person.favoriteMovie}</li>
-        <li>{person.favoriteMusic}</li>
-      </ul>
-      </div>
-      <div className="card">
-        <h2>Hobbies</h2>
-        <ul>
-          <li>{person.hobbies[0]}</li>
-          <li>{person.hobbies[1]}</li>
-          <li>{person.hobbies[2]}</li>
-          <li>{person.hobbies[3]}</li>
-          <li>{person.hobbies[4]}</li>
-        </ul>
-      </div>
-      
-      <div className="card">
-        <h2>Mi Stack Tech</h2>
-        <ul>
-          <li>{person.stack[0]}</li>
-          <li>{person.stack[1]}</li>
-          <li>{person.stack[2]}</li>
-          <li>{person.stack[3]}</li>
-          <li>{person.stack[4]}</li>
-        </ul>
-      </div>
+        <Personal />
 
-      <FavoriteFood />
+        <Hobbies />
+
+        <FavoriteFood />
+        
+        <StackTech />
       </div>
     </div>
   )
